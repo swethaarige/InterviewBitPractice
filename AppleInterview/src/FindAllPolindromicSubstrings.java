408060f206df6ddeaa2f8386b4d821a139cc21db
swetha <swetha.arige@gmail.com>
2022-02-03 21:13:11 -0800

    Apple Practice

---
+++ AppleInterview/src/FindAllPolindromicSubstrings.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Find all possible palindromic substrings of a string
+The problem differs from the problem of finding the possible palindromic subsequence.
+Unlike subsequences, substrings are required to occupy consecutive positions within the original string.
+For example, Input:  str = google Output: e l g o oo goog
+A simple solution would be to generate all substrings of the given string and print substrings
+that are palindromes. The time complexity of this solution would be O(n3), where n is the length
+of the input string.
+We can solve this problem in O(n2) time and O(1) space. The idea is inspired by the Longest
+Palindromic Substring problem. For each character in the given string, consider it as the midpoint
+of a palindrome and expand in both directions to find all palindromes that have it as midpoint.
+For an even length palindrome, consider every adjacent pair of characters as the midpoint.
+We use a set to store all unique palindromic substrings.
+ */
+public class FindAllPolindromicSubstrings {
+    public static Set<String> findPalindromicSubstrings(String str) {
+        Set<String> set = new HashSet<>();
+        //Base case
+        if (str == null) {
+            return set;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            // find all odd length palindrome with `str[i]` as a midpoint
+            expand(str, i, i, set);
+            // find all even length palindrome with `str[i]` and `str[i+1]`
+            // as its midpoints
+            expand(str, i, i + 1, set);
+        }
+        // print all unique palindromic substrings
+        System.out.print(set);
+        return set;
+    }
+    // Expand in both directions of `low` and `high` to find all palindromes
+    public static void expand(String string, int low, int high, Set<String> setOfStrings) {
+        // run till `str[low.high]` is not a palindrome
+        while (low >= 0 && high < string.length() &&
+                string.charAt(low) == string.charAt(high)) {
+            setOfStrings.add(string.substring(low, high + 1));
+            //Expand In both directions
+            low--;
+            high++;
+        }
+    }
+    public static void main(String[] args)
+    {
+        String str = "google";
+
+        findPalindromicSubstrings(str);
+    }
+}
